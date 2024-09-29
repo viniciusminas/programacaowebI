fe7e2dade52903dac79de05138c50b4363d926a6
viniciusminas <viniciusminas_@hotmail.com>
2024-09-29 20:26:06 -0300

    Update exercicio5.php

--- FolhaExercicios/exercicio5.php
+++ FolhaExercicios/exercicio5.php
@@ -1,9 +1,3 @@
-//Crie um programa que calcule a área de um triângulo retângulo, cuja fórmula segue
-//abaixo. Crie as variáveis apropriadas para o cálculo em PHP e por fim exiba uma frase
-//com o valor da operação.
-
-//Fórmula -> Resultado = (Base * Altura) / 2
-
 <!DOCTYPE html>
 <html lang="PT-BR">
 <head>
