7a1f1c067b724be4bbc80227a370faff1799760f
viniciusminas <viniciusminas_@hotmail.com>
2024-10-04 18:48:44 -0300

    Update exercicios10.php

--- FolhaExercicios/exercicios10.php
+++ FolhaExercicios/exercicios10.php
@@ -1,24 +1,56 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Árvore Diretórios</title>
+    <style>
+        ul {
+            list-style-type: none;
+        }
+        li {
+            margin-left: 20px;
+        }
+    </style>
+</head>
+<body>
 
+<h1>Estrutura de Pastas</h1>
+
+<?php
 $pastas = array(
     "bsn" => array(
-        "3a Fase" => array("desenvWeb", "bancoDados 1", "engSoft 1"),
-        "4a Fase" => array("Intro Web", "bancoDados 2", "engSoft 2")
+        "3a Fase" => array(
+            "desenvWeb",
+            "bancoDados 1",
+            "engSoft 1"
+        ),
+        "4a Fase" => array(
+            "Intro Web",
+            "bancoDados 2",
+            "engSoft 2"
+        )
     )
 );
 
-function exibirArvore($array, $nivel = 0) {
-    foreach ($array as $key => $value) {
-        echo str_repeat("-", $nivel) . " " . $key . "<br>";
-
-        if (is_array($value)) {
-            exibirArvore($value, $nivel + 1);
+function exibirArvore($pastas) {
+    echo "<ul>"; 
+    foreach ($pastas as $pasta => $conteudo) {
+        
+        if (is_array($conteudo)) {
+         
+            echo "<li>$pasta</li>";
+            exibirArvore($conteudo); 
         } else {
-            echo str_repeat("-", $nivel + 1) . " " . $value . "<br>";
+
+            echo "<li>$conteudo</li>";
         }
     }
+    echo "</ul>"; 
 }
 
 exibirArvore($pastas);
-
 ?>
+
+</body>
+</html>
